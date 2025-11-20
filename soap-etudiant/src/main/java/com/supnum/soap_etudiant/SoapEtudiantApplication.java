2aec2319b41c5ae446d46e56f5a518936ef273ac
Aichetou05 <23021@supnum.mr>
2025-11-20 21:38:11 +0100

    Instialiser le projet

---
+++ soap-etudiant/src/main/java/com/supnum/soap_etudiant/SoapEtudiantApplication.java
@@ -0,0 +1,13 @@
+package com.supnum.soap_etudiant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapEtudiantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapEtudiantApplication.class, args);
+	}
+
+}
