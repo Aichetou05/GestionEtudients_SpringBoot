97f4091d56fd772aa0d262988f788738a489d2ff
Aichetou05 <23021@supnum.mr>
2025-11-20 22:13:31 +0100

    intialiser le projet

---
+++ soap-etudiant/src/test/java/com/supnum/soap_etudiant/SoapEtudiantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supnum.soap_etudiant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapEtudiantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
