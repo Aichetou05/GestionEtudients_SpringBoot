10a0b724bd3eb228c0183316f845c49030ff0858
Aichetou05 <23021@supnum.mr>
2025-11-20 21:31:33 +0100

    vider le branche

--- src/main/java/supnum_api/supnum/Controllers/EtudiantNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package supnum_api.supnum.Controllers;
-
-public class EtudiantNotFoundException extends RuntimeException {
-    public EtudiantNotFoundException(Long id) {
-        super("Étudiant introuvable avec id=" + id);
-    }
-}
-
